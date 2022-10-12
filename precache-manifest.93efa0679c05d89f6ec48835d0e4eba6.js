103c4ba0917a2fca41394e4261e1dbd74b7af665
Steve Yalcin <steveyalcin@Steves-MacBook-Pro.local>
2022-10-12 17:59:54 -0400

    Updates

---
+++ precache-manifest.93efa0679c05d89f6ec48835d0e4eba6.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f6b96ee08acffd12f573a24ea8dcf8f",
+    "url": "/react-formula1-api/index.html"
+  },
+  {
+    "revision": "51aaa01874036272ce62",
+    "url": "/react-formula1-api/static/css/main.042dfffa.chunk.css"
+  },
+  {
+    "revision": "c4cb0051e4a979e5ed60",
+    "url": "/react-formula1-api/static/js/2.ed5c6674.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-formula1-api/static/js/2.ed5c6674.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51aaa01874036272ce62",
+    "url": "/react-formula1-api/static/js/main.5903ad13.chunk.js"
+  },
+  {
+    "revision": "388010ec465a23d47d15",
+    "url": "/react-formula1-api/static/js/runtime-main.041415c1.js"
+  },
+  {
+    "revision": "8c067775d69dad5f6fa67fb919840271",
+    "url": "/react-formula1-api/static/media/adrian-sutil.8c067775.jpg"
+  },
+  {
+    "revision": "9be1415606ec0402d594abc0b695e64f",
+    "url": "/react-formula1-api/static/media/anthony-davidson.9be14156.jpeg"
+  },
+  {
+    "revision": "a63c795bdaf416985061dd536dc95aaf",
+    "url": "/react-formula1-api/static/media/bmw.a63c795b.png"
+  },
+  {
+    "revision": "8f1a419a0fafb8c47288dc1c8aaf24ce",
+    "url": "/react-formula1-api/static/media/david-coulthard.8f1a419a.jpg"
+  },
+  {
+    "revision": "019d4962a3a8c3916089c36425fb3983",
+    "url": "/react-formula1-api/static/media/felipe-massa.019d4962.jpg"
+  },
+  {
+    "revision": "7e13e9fec04b6b6ea152ec944329c10e",
+    "url": "/react-formula1-api/static/media/fernando-alonso.7e13e9fe.jpg"
+  },
+  {
+    "revision": "8313394803b6aa509035632ee5c63203",
+    "url": "/react-formula1-api/static/media/ferrari.83133948.png"
+  },
+  {
+    "revision": "a8655c173a076115c75c3ca76fd2a911",
+    "url": "/react-formula1-api/static/media/force-india.a8655c17.png"
+  },
+  {
+    "revision": "a2088a47a9c51f823947463721f50433",
+    "url": "/react-formula1-api/static/media/giancarlo-fisichella.a2088a47.jpg"
+  },
+  {
+    "revision": "6cadba564ae854a7b01054a5c505681c",
+    "url": "/react-formula1-api/static/media/heikki-kovalainen.6cadba56.jpg"
+  },
+  {
+    "revision": "93e52f78272f0e26b3253235861c94a2",
+    "url": "/react-formula1-api/static/media/honda.93e52f78.png"
+  },
+  {
+    "revision": "4c2506df876d021a6d463775760771cb",
+    "url": "/react-formula1-api/static/media/jarno-trulli.4c2506df.jpg"
+  },
+  {
+    "revision": "58eb667a8318626f31948fe06086dc5f",
+    "url": "/react-formula1-api/static/media/jenson-button.58eb667a.jpg"
+  },
+  {
+    "revision": "bdebaaf0a609b8c90937bbaac0c7725f",
+    "url": "/react-formula1-api/static/media/kazuki-nakajima.bdebaaf0.jpg"
+  },
+  {
+    "revision": "76378ba41e2beb5d4cc6b518205b94bd",
+    "url": "/react-formula1-api/static/media/kimi-raikkonen.76378ba4.jpg"
+  },
+  {
+    "revision": "3e618ba818518880c14d3564969211cc",
+    "url": "/react-formula1-api/static/media/lewis-hamilton.3e618ba8.jpg"
+  },
+  {
+    "revision": "945ed8c1f514dfd567c96545e3ad6d4a",
+    "url": "/react-formula1-api/static/media/mark-webber.945ed8c1.jpg"
+  },
+  {
+    "revision": "6fa886c2824dfbcef48ebf337630417f",
+    "url": "/react-formula1-api/static/media/mclaren.6fa886c2.png"
+  },
+  {
+    "revision": "2d30b09f11bd63feaafd7ffe4a6da5ce",
+    "url": "/react-formula1-api/static/media/michael-schumacher.2d30b09f.jpg"
+  },
+  {
+    "revision": "73bab3015a8b34ff010a7b1cffa65e83",
+    "url": "/react-formula1-api/static/media/nelson-piquet-jr.73bab301.jpg"
+  },
+  {
+    "revision": "212c6ac6d3a9f919609221703e1e6e5a",
+    "url": "/react-formula1-api/static/media/nick-heidfeld.212c6ac6.jpg"
+  },
+  {
+    "revision": "44a64dc93f0390e40dc3ed1610b5a379",
+    "url": "/react-formula1-api/static/media/nico-rosberg.44a64dc9.png"
+  },
+  {
+    "revision": "b263f0291c98b80ad5bf42b36526d109",
+    "url": "/react-formula1-api/static/media/red-bull.b263f029.png"
+  },
+  {
+    "revision": "2407a25a11f7b2d3de0898b324698d36",
+    "url": "/react-formula1-api/static/media/renault.2407a25a.png"
+  },
+  {
+    "revision": "f07d0561cb93f9c3c34ef2f5f7528ee0",
+    "url": "/react-formula1-api/static/media/robert-kubica.f07d0561.jpg"
+  },
+  {
+    "revision": "6d07838822b10d08eea3be9210b7e2ae",
+    "url": "/react-formula1-api/static/media/rubens-barrichello.6d078388.jpg"
+  },
+  {
+    "revision": "92d4e5420296f2dd94e8b0b075513fa7",
+    "url": "/react-formula1-api/static/media/schumacher-banner.92d4e542.jpg"
+  },
+  {
+    "revision": "53ff68cff4fc5ac635f2fe1a5ba15ee1",
+    "url": "/react-formula1-api/static/media/sebastian-vettel.53ff68cf.jpg"
+  },
+  {
+    "revision": "a96a033c1e103ed117e76d791e7d6b00",
+    "url": "/react-formula1-api/static/media/sebastien-bourdais.a96a033c.jpg"
+  },
+  {
+    "revision": "897d28953f3b90b81a0d153ff2f8066c",
+    "url": "/react-formula1-api/static/media/super-aguri.897d2895.png"
+  },
+  {
+    "revision": "35f619348c10c8feb6a04f00b1bad132",
+    "url": "/react-formula1-api/static/media/timo-glock.35f61934.jpg"
+  },
+  {
+    "revision": "fe9f13a51ee53fa5198f0680e0704846",
+    "url": "/react-formula1-api/static/media/tire.fe9f13a5.png"
+  },
+  {
+    "revision": "9aaaa65eeeb55c9bc459a04603c13745",
+    "url": "/react-formula1-api/static/media/toro-rosso.9aaaa65e.png"
+  },
+  {
+    "revision": "c3b7d2b364012c40c6f73734589763ec",
+    "url": "/react-formula1-api/static/media/toyota.c3b7d2b3.png"
+  },
+  {
+    "revision": "7dcd8223ec2d4753db752614a7f7f7bd",
+    "url": "/react-formula1-api/static/media/williams.7dcd8223.png"
+  }
+]);
