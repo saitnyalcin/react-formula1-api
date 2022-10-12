7f493cee61c5656d7cb9d92a3b900076466d091d
Steve Yalcin <steveyalcin@Steves-MacBook-Pro.local>
2022-10-12 17:49:54 -0400

    codebase style cleaning

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import logo from "./images/tire.png";
 import f1logo from "./images/F1_logo.png";
 import "./App.css";
-import "./style.scss";
 import Formula from "./components/Formula";
 
 function App() {
